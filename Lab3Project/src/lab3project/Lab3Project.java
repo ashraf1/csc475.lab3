0c0f5955be813e6aca30e317a776f3ac84f66c3f
ash <ashfadili@hotmail.com>
2013-09-18 13:03:47 -0400

    first commit

---
+++ Lab3Project/src/lab3project/Lab3Project.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab3project;
+
+
+/**
+ *
+ * @author s0232472
+ */
+public class Lab3Project
+{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main( String[] args )
+    {
+        // TODO code application logic here
+    }
+}
